2fd6083686caa34bc09a856591e938b8a6923700
Jim Kang <jimkang@gmail.com>
2016-06-04 23:15:42 -0400

    Trim space from link-finding URL desc.

--- get-link-finding-image.js
+++ get-link-finding-image.js
@@ -81,7 +81,7 @@ function GetLinkFindingImage(opts) {
 function getLinkFindingURL(imageConceptResult) {
   linkFindingURL = baseLinkRenderURL;
   linkFindingURL += encodeURIComponent(imageConceptResult.imgurl);
-  linkFindingURL += '/desc/' + imageConceptResult.concept;
+  linkFindingURL += '/desc/' + imageConceptResult.concept.trim();
 
   if (imageConceptResult.width) {
     linkFindingURL += '/width/' + imageConceptResult.width;
